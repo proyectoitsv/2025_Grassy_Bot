2c6a39b90b8d29f21c37726dd446627fe1d96b71
AgusIzqui <agustinizqu@gmail.com>
2025-11-12 14:08:12 -0300

    Add files via upload

--- CODIGO/Grassy_Bot_Main/Grassy_Bot_Main.ino
+++ CODIGO/Grassy_Bot_Main/Grassy_Bot_Main.ino
@@ -1,30 +1,34 @@
 #include <Arduino.h>
 #include <BluetoothSerial.h>
-#include <Adafruit_Sensor.h>
+#include <LiquidCrystal_I2C.h>
 #include <Wire.h>
+#include <PCF8574.h>
 #include <Adafruit_MPU6050.h>
-
-// Sensores
-Adafruit_MPU6050 mpu;
-
+#include <Adafruit_Sensor.h>
+LiquidCrystal_I2C lcd(0x27,16,2); // si no te sale con esta direccion  puedes usar (0x3f,16,2) || (0x27,16,2)  ||(0x20,16,2) 
 BluetoothSerial SerialBT;
+PCF8574 pcf8574(0x24);
+Adafruit_MPU6050 mpu;
 String receivedData = "";
-
 #define StepD 27
 #define StepI 13
 #define DirD 26
 #define DirI 14
 #define Motor 2
-#define LED_INTEGRADO 2
+#define SDA_PIN 21
+#define SCL_PIN 22
 
-// Variables globales
-float roll, pitch;
 
-int modo = 0;  //0 Esperando /1 Manual /2 Automatico
+int modo = 0; //0 Esperando /1 Manual /2 Automatico
 bool cuchilla = 0;
 int ancho = 0;
 int largo = 0;
+float roll = 0;
+float pitch = 0;
 hw_timer_t *timer1 = NULL;
+
+bool Bandera1 = 0;
+
 
 // Declaraciones de funciones
 void avanzar(int repeticiones);
@@ -37,49 +41,54 @@ void frente();
 void reversa();
 void zurdo();
 void diestro();
+void expansor();
+void inclinacion();
 void IRAM_ATTR onTimer1();
-void leerSensores();
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("Grassy Bot 0.6");
+  Wire.begin(SDA_PIN, SCL_PIN);
+  pcf8574.begin();
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0,0);
+  SerialBT.begin("Grassy Bot 0.8");
   Serial.println("Bluetooth iniciado. Listo para emparejar!");
-
+  lcd.print(  "GrassyBot 8");
+  lcd.display();
   pinMode(StepD, OUTPUT);
   pinMode(StepI, OUTPUT);
   pinMode(DirD, OUTPUT);
   pinMode(DirI, OUTPUT);
   pinMode(Motor, OUTPUT);
-  pinMode(LED_INTEGRADO, OUTPUT);
 
-  Wire.begin();
-
-  // INICIALIZAR MPU6050
   if (!mpu.begin()) {
-    Serial.println("Error: No se pudo inicializar MPU6050!");
-    while (1) {
-      
-    }
+    lcd.setCursor(0,1);
+    lcd.println(" MPU6050 Error  ");
   }
-  Serial.println("MPU6050 inicializado correctamente");
-
-  // Configurar rango del MPU6050
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-
-  //TIMER
-  timer1 = timerBegin(1000000);  // frequency en Hz
+  
+  timer1 = timerBegin(1000000); // frequency en Hz
   timerAttachInterrupt(timer1, &onTimer1);
   timerAlarm(timer1, 1000000ULL, true, 0);
-
-  Serial.println("Sistema listo!");
+  
 }
 
 void loop() {
-  leerSensores();
 
-  //ZONA COMANDOS BLUETOOTH
+//ZONA COMANDOS BLUETOOTH
+
+  inclinacion();
+  lcd.setCursor(0,1);
+  lcd.print("R: ");
+  lcd.print(roll);
+  lcd.print("P: ");
+  lcd.print(pitch);
+  lcd.print("      ");
+
   if (SerialBT.available()) {
     char c = SerialBT.read();
     receivedData += c;
@@ -87,75 +96,57 @@ void loop() {
 
     if (c == '\n') {
       receivedData.trim();
-
-      if (modo == 0) {
+      
+      if(modo == 0){
         if (receivedData.startsWith("MAN")) {
           modo = 1;
-          Serial.println("Modo MANUAL activado");
         }
         if (receivedData.startsWith("AUT")) {
           modo = 2;
-          Serial.println("Modo AUTOMATICO activado");
         }
-      } else if (modo == 1) {
+      }
+      else if(modo == 1){
         if (receivedData.startsWith("AVA")) {
           String numeroStr = receivedData.substring(3);
           int x = numeroStr.toInt();
-          Serial.print("Avanzar: ");
-          Serial.println(x);
           avanzar(x);
         }
         if (receivedData.startsWith("REV")) {
           String numeroStr = receivedData.substring(3);
           int x = numeroStr.toInt();
-          Serial.print("Retroceder: ");
-          Serial.println(x);
           retroceder(x);
         }
         if (receivedData.startsWith("DER")) {
           String numeroStr = receivedData.substring(3);
           int x = numeroStr.toInt();
-          Serial.print("Derecha: ");
-          Serial.println(x);
           derecha(x);
         }
         if (receivedData.startsWith("IZQ")) {
           String numeroStr = receivedData.substring(3);
           int x = numeroStr.toInt();
-          Serial.print("Izquierda: ");
-          Serial.println(x);
           izquierda(x);
         }
         if (receivedData.startsWith("MOT")) {
           cuchilla = !cuchilla;
-          //digitalWrite(Motor, cuchilla);
-          digitalWrite(LED_INTEGRADO, HIGH);
-          Serial.print("Motor: ");
-          Serial.println(cuchilla ? "ON" : "OFF");
+          digitalWrite(Motor, cuchilla);
         }
         if (receivedData.startsWith("ESP")) {
           modo = 0;
-          Serial.println("Modo ESPERA activado");
         }
       }
-      if (modo == 2) {
+      if(modo == 2){
         if (receivedData.startsWith("ANC")) {
           String numeroStr = receivedData.substring(3);
           int x = numeroStr.toInt();
           ancho = x;
-          Serial.print("Ancho: ");
-          Serial.println(ancho);
         }
         if (receivedData.startsWith("LAR")) {
           String numeroStr = receivedData.substring(3);
           int x = numeroStr.toInt();
           largo = x;
-          Serial.print("Largo: ");
-          Serial.println(largo);
         }
         if (receivedData.startsWith("ESP")) {
           modo = 0;
-          Serial.println("Modo ESPERA activado");
         }
       }
       receivedData = "";
@@ -163,121 +154,119 @@ void loop() {
     }
   }
 
-  if (Serial.available()) {  //Mandar Serial por BT
+  if (Serial.available()) {//Mandar Serial por BT
     char c = Serial.read();
     SerialBT.write(c);
     if (c == '\n') {
       SerialBT.flush();
     }
   }
+
 }
 
-// FUNCIONES MPU6050
-void leerSensores() {
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
-  roll = atan2(a.acceleration.y, a.acceleration.z) * 180 / PI;
-  pitch = atan2(-a.acceleration.x, sqrt(a.acceleration.y * a.acceleration.y + a.acceleration.z * a.acceleration.z)) * 180 / PI;
-
-  if (abs(roll) > 30 || abs(pitch) > 30) {
-    // Se detectó inclinación mayor a 30 grados
-    digitalWrite(Motor, HIGH);
-    cuchilla = 1;
-    Serial.println("ALERTA: Inclinación detectada! Motor encendido");
-    Serial.print("Roll: ");
-    Serial.print(roll);
-    Serial.print(" Pitch: ");
-    Serial.println(pitch);
-  }
-  digitalWrite(Motor, LOW);
-}
 
 //COMANDOS
-void avanzar(int repeticiones) {
-  frente();
-  if (repeticiones == 0) {
-    repeticiones = 1;
-  }
-  for (int x = 0; x < repeticiones; x++) {
-    paso();
-  }
-}
 
-void retroceder(int repeticiones) {
-  reversa();
-  if (repeticiones == 0) {
-    repeticiones = 1;
+  void avanzar(int repeticiones) {
+    frente();
+    if (repeticiones == 0) {
+      repeticiones = 1;
+    }
+    for (int x = 0; x < repeticiones; x++) {
+      paso();
+    }
   }
-  for (int x = 0; x < repeticiones; x++) {
-    paso();
-  }
-}
 
-void izquierda(int repeticiones) {
-  zurdo();
-  if (repeticiones == 0) {
-    repeticiones = 1;
+  void retroceder(int repeticiones) {
+    reversa();
+    if (repeticiones == 0) {
+      repeticiones = 1;
+    }
+    for (int x = 0; x < repeticiones; x++) {
+      paso();
+    }
   }
-  for (int x = 0; x < repeticiones; x++) {
-    giro();
-  }
-}
 
-void derecha(int repeticiones) {
-  diestro();
-  if (repeticiones == 0) {
-    repeticiones = 1;
+  void izquierda(int repeticiones) {
+    zurdo();
+    if (repeticiones == 0) {
+      repeticiones = 1;
+    }
+    for (int x = 0; x < repeticiones; x++) {
+      giro();
+    }
   }
-  for (int x = 0; x < repeticiones; x++) {
-    giro();
+
+  void derecha(int repeticiones) {
+    diestro();
+    if (repeticiones == 0) {
+      repeticiones = 1;
+    }
+    for (int x = 0; x < repeticiones; x++) {
+      giro();
+    }
   }
-}
 
 //MOVERSE
-void paso() {
-  for (int x = 0; x < 200; x++) {
-    digitalWrite(StepD, HIGH);
-    digitalWrite(StepI, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(StepD, LOW);
-    digitalWrite(StepI, LOW);
-    delayMicroseconds(1000);
-  }
-}
 
-void giro() {
-  for (int x = 0; x < 200; x++) {
-    digitalWrite(StepD, HIGH);
-    digitalWrite(StepI, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(StepD, LOW);
-    digitalWrite(StepI, LOW);
-    delayMicroseconds(1000);
+  void paso() {
+    for (int x = 0; x < 200; x++) {
+      digitalWrite(StepD, HIGH);
+      digitalWrite(StepI, HIGH);
+      delayMicroseconds(1000);
+      digitalWrite(StepD, LOW);
+      digitalWrite(StepI, LOW);
+      delayMicroseconds(1000);
+    }
   }
-}
+
+  void giro() {
+    for (int x = 0; x < 200; x++) {
+      digitalWrite(StepD, HIGH);
+      digitalWrite(StepI, HIGH);
+      delayMicroseconds(1000);
+      digitalWrite(StepD, LOW);
+      digitalWrite(StepI, LOW);
+      delayMicroseconds(1000);
+    }
+  }
 
 //DIRECCIONES
-void frente() {
-  digitalWrite(DirD, HIGH);
-  digitalWrite(DirI, LOW);
-}
 
-void reversa() {
-  digitalWrite(DirD, LOW);
-  digitalWrite(DirI, HIGH);
-}
+  void frente() {
+    digitalWrite(DirD, HIGH);
+    digitalWrite(DirI, LOW);
+  }
+  void reversa() {
+    digitalWrite(DirD, LOW);
+    digitalWrite(DirI, HIGH);
+  }
+  void zurdo() {
+    digitalWrite(DirD, HIGH);
+    digitalWrite(DirI, HIGH);
+  }
+  void diestro() {
+    digitalWrite(DirD, LOW);
+    digitalWrite(DirI, LOW);
+  }
 
-void zurdo() {
-  digitalWrite(DirD, HIGH);
-  digitalWrite(DirI, HIGH);
-}
+//TIMERS
+  
+  void IRAM_ATTR onTimer1() {
+    
+  }
 
-void diestro() {
-  digitalWrite(DirD, LOW);
-  digitalWrite(DirI, LOW);
-}
+//SENSORES
 
-//TIMER VOIDS
-void IRAM_ATTR onTimer1() {
-  // Timer para funciones futuras
-}
+  void expansor(){
+    
+  }
+
+  void inclinacion(){
+
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+    roll = atan2(a.acceleration.y, a.acceleration.z) * 180 / PI;
+    pitch = atan2(-a.acceleration.x, sqrt(a.acceleration.y * a.acceleration.y + a.acceleration.z * a.acceleration.z)) * 180 / PI;
+
+  }
